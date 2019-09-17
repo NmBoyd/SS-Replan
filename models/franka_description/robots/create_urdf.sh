aca964b1ee83c299d2b01b221b570d649ed52d5d
Chris Paxton <cpaxton@nvidia.com>
2019-09-17 13:42:07 -0700

    Final robot commit

---
+++ models/franka_description/robots/create_urdf.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d21a5671fa3f5945015a685ee032d920f2695f96684c1bf69a904bbb4998f8a7
+size 1006
---
+++ models/kitchen_description/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a064758efa9880177a4766053bb2bf7d39f51c73a86d313b6fc394ddcb5c0b8
+size 277
